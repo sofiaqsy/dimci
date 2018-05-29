3a8b3b7ebe4af9b48b5b89c53cf9cb567e79b3f6
sofiaqsy <keyl.qsy@gmail.com>
2018-05-29 15:57:47 -0500

    corregido

--- app/src/main/java/com/halo/loginui2/vistas/Activity_Login.java
+++ app/src/main/java/com/halo/loginui2/vistas/Activity_Login.java
@@ -1,6 +1,14 @@
-<<<<<<< HEAD:app/src/main/java/com/halo/loginui2/Activity_Login.java
-package com.halo.loginui2;
+package com.halo.loginui2.vistas;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Button;
+
+import com.halo.loginui2.R;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Handler;
@@ -157,53 +165,3 @@ public class Activity_Login extends AppCompatActivity {
         return valid;
     }
 }
-=======
-package com.halo.loginui2.vistas;
-
-import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.RelativeLayout;
-import android.widget.Button;
-
-import com.halo.loginui2.R;
-
-
-public class Activity_Login extends AppCompatActivity {
-
-    RelativeLayout rellay1, rellay2;
-    Button register;
-
-    Handler handler = new Handler();
-    Runnable runnable = new Runnable() {
-        @Override
-        public void run() {
-            rellay1.setVisibility(View.VISIBLE);
-            rellay2.setVisibility(View.VISIBLE);
-        }
-    };
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        rellay1 = (RelativeLayout) findViewById(R.id.rellay1);
-        rellay2 = (RelativeLayout) findViewById(R.id.rellay2);
-
-        handler.postDelayed(runnable, 2000); //2000 is the timeout for the splash
-
-        register = (Button) findViewById(R.id.register);
-
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent register = new Intent(Activity_Login.this, Activity_Register.class);
-                startActivity(register);
-            }
-        });
-    }
-}
->>>>>>> f75cc0ace7a0fc7700f75a4376bcf31027f97958:app/src/main/java/com/halo/loginui2/vistas/Activity_Login.java
